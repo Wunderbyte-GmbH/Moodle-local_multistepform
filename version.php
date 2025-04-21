23b4e64741bb0d90f40085ed4423e6ca2d09b4c9
Georg Maißer <georg.maisser@wunderbyte.at>
2025-04-21 14:33:26 +0200

    Version 1.0.0 (2025042100)

--- version.php
+++ version.php
@@ -25,5 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_multistepform';
-$plugin->version = 2025041304;
+$plugin->release = '1.0.0';
+$plugin->version = 2025042100;
 $plugin->requires = 2022112800;
