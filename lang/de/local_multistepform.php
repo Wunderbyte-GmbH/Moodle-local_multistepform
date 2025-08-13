bba0a6d4d206c1c7d72510641d23da73facd6dd2
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2025-08-13 08:08:10 +0200

    Improvement: Included german strings

---
+++ lang/de/local_multistepform.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_multistepform'.
+ *
+ * @package   local_multistepform
+ * @copyright 2025
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['cachedef_multistepform'] = 'Cache für mehrstufiges Formular';
+$string['ischecked'] = 'ist ausgewählt';
+$string['isnotchecked'] = 'ist nicht ausgewählt';
+$string['nofieldsreview'] = 'Es gibt keine Daten zum Überprüfen.';
+$string['pluginname'] = 'Mehrstufiges Formular';
+$string['reviewintro'] = 'Bitte überprüfen Sie die untenstehenden Angaben, bevor Sie das Formular absenden.';
+$string['reviewtitle'] = 'Überprüfen Sie Ihre Angaben';
+$string['step'] = 'Schritt {$a}';
